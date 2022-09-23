f8c63253581291a4bd44e37039c392f321d90cae
larry.huang <larry.huang@scmp.com>
2022-09-23 10:23:58 +0800

    feat: add a new button with different event parameters

---
+++ src/ButtonWrapper.tsx
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+const ButtonWrapper = styled.div`
+  padding: 8px 0;
+`
+
+export default ButtonWrapper
--- src/utils.ts
+++ src/utils.ts
@@ -47,3 +47,53 @@ export function pushGA4Event(args: PushGA4EventArgs): void {
 
   dataLayer.push(parameters)
 }
+
+interface PushGA4EventNameArgs {
+  campaignName?: string
+  event_name: string
+  followName?: string
+  followType?: string
+  method?: string
+  newsletterName?: string
+  paywallType?: string
+  triggerPoint?: string
+}
+
+export function pushGA4EventName(args: PushGA4EventNameArgs): void {
+  const dataLayer = (window as any).dataLayer
+  if (!dataLayer) {
+    return
+  }
+
+  const {
+    campaignName,
+    event_name,
+    followName,
+    followType,
+    method,
+    newsletterName,
+    paywallType,
+    triggerPoint,
+  } = args
+
+  const requiredParameters = {
+    event_name,
+  }
+
+  const optionalParameters = {
+    ...(campaignName ? { campaign_name: campaignName } : {}),
+    ...(followName ? { follow_name: followName } : {}),
+    ...(followType ? { follow_type: followType } : {}),
+    ...(method ? { method: method } : {}),
+    ...(newsletterName ? { newsletter_name: newsletterName } : {}),
+    ...(paywallType ? { paywall_type: paywallType } : {}),
+    ...(triggerPoint ? { trigger_point: triggerPoint } : {}),
+  }
+
+  const parameters = {
+    ...requiredParameters,
+    ...optionalParameters,
+  }
+
+  dataLayer.push(parameters)
+}
