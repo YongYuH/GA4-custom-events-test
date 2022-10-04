c9291d014b0b1046503b17753011032336798be5
larry.huang <larry.huang@scmp.com>
2022-10-04 17:36:58 +0800

    feat: update event parameters

--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,3 @@
-type PopupEvent = 'login_popup_click' | 'login_popup_close_click' | 'login_popup_imp'
-type TriggerPoint = 'pop up open button' | 'email button' | 'pop up close button'
-type PopupEventParameters = {
-  event: PopupEvent
-  trigger_point: TriggerPoint
-}
-
 type EmailEvent = 'login_email_back_click' | 'login_email_forget_password_click'
 type EmailEventParameters = {
   event: EmailEvent
@@ -15,7 +8,31 @@ type ForgetPasswordEventParameters = {
   event: ForgetPasswordEvent
 }
 
-type PushGA4EventArgs = PopupEventParameters | EmailEventParameters | ForgetPasswordEventParameters
+type LoginMethodClickEvent = 'login_popup_click'
+type Method = 'email'
+type LoginMethodClickEventParameters = {
+  event: LoginMethodClickEvent
+  method: Method
+}
+
+type PopupControlEvent = 'login_popup_close_click'
+type PopupControlEventParameters = {
+  event: PopupControlEvent
+}
+
+type PopupImpressionEvent = 'login_popup_imp'
+type TriggerPoint = 'pop up open button'
+type PopupImpressionEventParameters = {
+  event: PopupImpressionEvent
+  trigger_point: TriggerPoint
+}
+
+type PushGA4EventArgs =
+  | EmailEventParameters
+  | ForgetPasswordEventParameters
+  | LoginMethodClickEventParameters
+  | PopupControlEventParameters
+  | PopupImpressionEventParameters
 
 export function pushGA4Event(args: PushGA4EventArgs): void {
   const dataLayer = (window as any).dataLayer
